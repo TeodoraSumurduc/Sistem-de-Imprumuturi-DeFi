cb5b45feef13c1ac8e1363fb6a16fb82773c9062
Sumururduc Alexandru <sumurduc2003@gmail.com>
2025-02-06 05:40:17 +0200

    Update0.85

--- contracts/DeFiLoan.sol
+++ contracts/DeFiLoan.sol
@@ -8,9 +8,16 @@ contract DeFiLoan is ILoan {
     mapping(address => Loan[]) public activeLoans;
     mapping(address => Loan[]) public paidLoans;
 
-    event LoanCreated(address indexed borrower, uint amount, uint interest, uint dueDate, bool isRepaid);
+    event LoanCreated(
+        address indexed borrower,
+        uint amount,
+        uint interest,
+        uint dueDate,
+        bool isRepaid
+    );
     event LoanRepaid(address indexed borrower, uint loanIndex);
     event OwnerAddress(address owner);
+    event LoanPayed(address indexed borrower, uint loanIndex);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the owner");
@@ -27,34 +34,42 @@ contract DeFiLoan is ILoan {
         require(_amount > 0, "Amount must be greater than 0");
         require(_dueDate > block.timestamp, "Due date must be in the future");
 
-        activeLoans[msg.sender].push(Loan({
-            amount: _amount,
-            interest: 10,
-            dueDate: _dueDate,
-            borrower: msg.sender,
-            isRepaid: false
-        }));
+        activeLoans[msg.sender].push(
+            Loan({
+                amount: _amount,
+                interest: 10,
+                dueDate: _dueDate,
+                borrower: msg.sender,
+                isRepaid: false
+            })
+        );
 
         emit LoanCreated(msg.sender, _amount, 10, _dueDate, false);
     }
 
     // Obține împrumuturile active ale unui utilizator
-    function getActiveLoans(address _borrower) external view override returns (Loan[] memory) {
+    function getActiveLoans(
+        address _borrower
+    ) external view override returns (Loan[] memory) {
         return activeLoans[_borrower];
     }
 
-    function getPaidLoans(address _borrower) external view returns (Loan[] memory) {
+    function getPaidLoans(
+        address _borrower
+    ) external view returns (Loan[] memory) {
         return paidLoans[_borrower];
     }
 
     // Permite rambursarea unui împrumut
+     // Permite rambursarea unui împrumut
     function repayLoan(address _borrower, uint256 _index) public payable override {
         require(_index < activeLoans[_borrower].length, "Loan does not exist");
 
         Loan storage loan = activeLoans[_borrower][_index];
         uint256 interestAmount = LoanLibrary.calculateInterest(loan.amount, loan.interest);
+        uint total = loan.amount + interestAmount;
 
-        require(msg.value >= interestAmount, "Not enough funds to repay loan");
+        require(msg.value == total, "Not enough funds to repay loan");
 
         // Marcare ca rambursat
         loan.isRepaid = true;
@@ -62,13 +77,10 @@ contract DeFiLoan is ILoan {
         // Transfer către owner
         payable(owner).transfer(interestAmount);
 
-        // Returnează surplusul utilizatorului
-        if (msg.value > interestAmount) {
-            payable(msg.sender).transfer(msg.value - interestAmount);
-        }
-
         paidLoans[_borrower].push(loan);
 
         emit LoanRepaid(_borrower, _index);
     }
 }
+   
+
